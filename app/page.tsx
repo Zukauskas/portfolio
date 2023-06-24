3fc8afd4b08c2e5c62eab5b6d63aa324c66a4855
Tautvydas Zukauskas <tautzuk@tutanota.com>
2023-06-24 23:07:05 +0300

    Add basic nav component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Nav from './components/Nav';
+
 export default function Home () {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>CONTENT</p>
-    </main>
+    <>
+      <Nav />
+      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <p>CONTENT</p>
+      </main>
+
+    </>
   );
 }
