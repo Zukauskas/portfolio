7ba28c19522605309920e5e42e7f9822e9a2d35d
Tautvydas Žukauskas <tautzuk@tutanota.com>
2023-06-28 02:38:31 +0300

    Add about me container and close button

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,22 @@
-import Image from 'next/image';
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function About () {
+export default function About (): JSX.Element {
   return (
     <>
       <main>
         <div className='min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4 pb-20'>
           <Image className='absolute top-0 left-0 w-full h-full bg-cover bg-center' src='/bg-image.jpg' alt='image with programming code' width='1920' height='1080' />
-          <h1 className='text-3xl md:text-5xl text-white font-bold mb-8 z-10'>Hi, my name is Tautvydas Žukauskas</h1>
-          <p className=' z-10 text-2xl text-white'>This is an about me page</p>
+          <div className='z-9 bg-white absolute top-5 bottom-2 w-10/12 rounded-lg'>
+            <div className=' border-b-2 border-black text-center flex items-center justify-center'>
+              <h1 className='text-xl md:text-2xl text-black font-bold z-10'>About Me</h1>
+              <Link className=' absolute right-3' href='/'>X</Link>
+            </div>
+            <p className=' z-10 text-2xl text-black'>This is an about me page</p>
+
+          </div>
         </div>
       </main>
     </>
-  );
+  )
 }
