33d7f6700cb12c363b5805446503864e6c69d011
Tautvydas Žukauskas <tautzuk@tutanota.com>
2023-06-28 04:58:11 +0300

    small styling fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,18 +6,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Personal website | Zukauskas.dev',
-  description: 'Tautvydas Žukauskas personal website',
+  description: 'Tautvydas Žukauskas personal website'
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout ({ children }: { children: React.ReactNode }): JSX.Element {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Nav/>
+        <Nav />
         {children}
       </body>
     </html>
