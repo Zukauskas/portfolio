25666d3be11c397f273230b44c14ca0f5afe2fa8
Tautvydas Žukauskas <tautzuk@tutanota.com>
2023-07-01 00:22:14 +0300

    Update card design

--- app/components/RepoCard.tsx
+++ app/components/RepoCard.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 
 interface Repository {
   id: number
-  name: string
+  full_name: string
   description: string
   html_url: string
+  language: string
 }
 
 interface RepoCardProps {
@@ -12,18 +14,16 @@ interface RepoCardProps {
 
 const RepoCard: React.FC<RepoCardProps> = ({ repo }) => {
   return (
-    <div className='bg-white rounded-lg shadow-lg p-4'>
-      <h3 className='text-xl font-bold mb-2'>{repo.name}</h3>
+    <div className='bg-gray-200 border border-gray-300 rounded-lg p-4 flex flex-col'>
+      <Link href={repo.html_url} className='text-lg font-semibold hover:underline mb-2'>{repo.full_name}
+      </Link>
       <p className='text-gray-700 mb-4'>{repo.description}</p>
-      <a
-        href={repo.html_url}
-        target='_blank'
-        rel='noopener noreferrer'
-        className='inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded'
-      >
-        Visit Repo
-      </a>
+      <div className='flex items-center'>
+        <span className='w-3 h-3 rounded-full bg-black mr-2' />
+        <span>{repo.language}</span>
+      </div>
     </div>
+
   )
 }
 
