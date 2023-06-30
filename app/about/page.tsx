c47760b82e8d75aa16941720fd3a37e4e51852bc
Tautvydas Žukauskas <tautzuk@tutanota.com>
2023-07-01 00:36:40 +0300

    Update description and close button

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ export default function About (): JSX.Element {
           <div className='z-9 bg-[#f8f8f2] absolute top-0 bottom-0 w-full rounded-xl sm:top-2 sm:bottom-2 sm:w-10/12 md:w-9/12 lg:w-10/12 overflow-scroll'>
             <div className=' border-b-2 border-[#44475a] bg-[#bd93f9] text-center flex items-center justify-center rounded-t-lg z-25'>
               <p className='text-xl md:text-2xl text-[#44475a] font-bold z-10'>About Me</p>
-              <Link className=' absolute right-3' href='/'>X</Link>
+              <Link className=' absolute right-3' href='/'><svg height='24px' viewBox='0 0 512 512' width='24px' xmlns='http://www.w3.org/2000/svg'><path d='M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z' /></svg></Link>
             </div>
             <div className='flex flex-col lg:flex-row gap-6 px-4'>
               <div className='bg-white shadow rounded-lg p-6 w-full lg:w-3/12'>
