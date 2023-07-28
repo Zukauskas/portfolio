14ca7bac542e8b8f04b3c89bb76e0e75f6725471
Tautvydas Z <tautzuk@tutanota.com>
2023-07-29 00:03:15 +0300

    Add formatting and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function Home (): JSX.Element {
-  return (
-    <>
-      <main>
-        <div className='min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4 pb-20'>
-          <Image className='absolute top-0 left-0 w-full h-full bg-cover bg-center' src='/bg-image.jpg' alt='image with programming code' width='1920' height='1080' />
-          <h1 className='text-3xl md:text-5xl text-white font-bold mb-8 z-10'>Hi, my name is Tautvydas Žukauskas</h1>
-          <p className=' z-10 text-2xl text-white'> And I am a web developer!</p>
-        </div>
-      </main>
-    </>
-  )
+export default function Home(): JSX.Element {
+    return (
+        <>
+            <main>
+                <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4 pb-20">
+                    <Image
+                        className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
+                        src="/bg-image.jpg"
+                        alt="image with programming code"
+                        width="1920"
+                        height="1080"
+                    />
+                    <div className="z-10 bg-black/50 pt-12 pb-12 pl-6 pr-6">
+                        <h1 className="text-3xl md:text-5xl text-white font-bold mb-8">
+                            Hi, my name is Tautvydas Žukauskas
+                        </h1>
+                        <p className="text-4xl text-white text-center">
+                            And I am a web developer!
+                        </p>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
