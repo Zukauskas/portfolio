0f64f2a6d7bbde1635aeeebd770115c28cf90c47
Tautvydas Z <tautzuk@tutanota.com>
2023-07-30 14:42:32 +0300

    Small image config change

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import code from "../public/bg-image.jpg";
 
 export default function Home(): JSX.Element {
     return (
@@ -7,10 +8,10 @@ export default function Home(): JSX.Element {
                 <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4 pb-20">
                     <Image
                         className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
-                        src="/bg-image.jpg"
+                        src={code}
                         alt="image with programming code"
-                        width="1920"
-                        height="1080"
+                        fill
+                        sizes="100vw"
                     />
                     <div className="z-10 bg-black/50 py-6 px-2">
                         <h1 className="text-md md:text-5xl md:py-12 md:px-6 animate-typingTitle overflow-hidden whitespace-nowrap text-white font-bold">
