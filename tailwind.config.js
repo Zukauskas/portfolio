39834895e310d929368ec04c22e57f3a1e99823b
Tautvydas Žukauskas <tautzuk@tutanota.com>
2023-09-24 16:54:54 +0300

    Update for nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-        extend: {
-            keyframes: {
-                typingTitle: {
-                    "0%": {
-                        width: "0%",
-                        visibility: "hidden",
-                    },
-                    "100%": {
-                        width: "100%",
-                    },
-                },
-            },
-            animation: {
-                typingTitle: "typingTitle 2s steps(20)",
-            },
+  theme: {
+    extend: {
+      keyframes: {
+        typingTitle: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
         },
+      },
+      animation: {
+        typingTitle: "typingTitle 2s steps(20)",
+      },
     },
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')],
-};
+  }
+}
