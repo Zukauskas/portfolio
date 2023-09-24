6f48a4c06afaf817abb446fc8f3fcea8a62f81a3
Tautvydas Žukauskas <tautzuk@tutanota.com>
2023-09-24 17:01:25 +0300

    Add next provider and use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+"use client";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import Projects from "./components/Projects";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home(): JSX.Element {
   return (
-    <>
+    <NextUIProvider>
       <Nav />
       <Hero />
       <About />
       <Projects />
-    </>
+    </NextUIProvider>
   );
 }
