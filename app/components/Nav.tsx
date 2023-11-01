9d288ba1fd89783a632873c34ea48e36e21e8c35
Tautvydas Z <118177903+Zukauskas@users.noreply.github.com>
2023-11-01 04:09:15 +0200

    Small fix

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,17 +1,7 @@
 "use client";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-} from "@nextui-org/react";
-import { usePathname } from "next/navigation";
+import { Navbar, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
 
 export default function Nav(): JSX.Element {
-  const pathName = usePathname();
-
   return (
     <Navbar>
       <NavbarContent className="hidden gap-4 sm:flex" justify="end">
