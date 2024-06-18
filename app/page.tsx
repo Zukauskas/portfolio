2d712efd6398f8d5043d07160c7551a12e2d66fc
Tautvydas Z <118177903+Zukauskas@users.noreply.github.com>
2024-06-19 00:09:14 +0300

    fix themeing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { LandingPage } from "@/components/landing-page";
 import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/md-dark-indigo/theme.css";
 
 export default function Home(): JSX.Element {
   return (
--- components/task-bar/TaskBar.tsx
+++ components/task-bar/TaskBar.tsx
@@ -5,31 +5,27 @@ import { Dock } from "primereact/dock";
 const Taskbar = () => {
   const dockItems = [
     {
-      icon: () => <img src="/task-bar-icons/user.png" width={69} />,
+      icon: () => <img src="/task-bar-icons/user.png" width="50pxs" />,
       label: "About Me",
       command: () => console.log("About Me"),
     },
     {
-      icon: () => <img src="/task-bar-icons/server.png" width={69} />,
+      icon: () => <img src="/task-bar-icons/server.png" width="50pxs" />,
       label: "Tech Stack",
       command: () => console.log("Tech Stack"),
     },
     {
-      icon: () => <img src="/task-bar-icons/folder.png" width={69} />,
+      icon: () => <img src="/task-bar-icons/folder.png" width="50pxs" />,
       label: "Projects",
       command: () => console.log("Projects"),
     },
     {
-      icon: () => <img src="/task-bar-icons/smartphone.png" width={69} />,
+      icon: () => <img src="/task-bar-icons/smartphone.png" width="50pxs" />,
       label: "Contacts",
       command: () => console.log("Contacts"),
     },
   ];
-  return (
-    <div>
-      <Dock model={dockItems} />;
-    </div>
-  );
+  return <Dock model={dockItems} />;
 };
 
 export default Taskbar;
